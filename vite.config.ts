06dd3be5f673f7e85c981edfdb54c0137ee391e8
adolfoespinozalopez <adolfoespinozalopez@gmail.com>
2026-05-15 17:01:20 -0500

    fix: correct base configuration in vite.config.ts for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,3 @@
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path' // 1. Importamos path para manejar rutas de carpetas
@@ -15,14 +14,15 @@ export default defineConfig({
     ],
   },
   plugins: [react()],
-  //base: '/key-investments-frontend/', // Configuración para GitHub Pages
+  base: '/key-investments-frontend/', // Configuración para GitHub Pages
+  //base: '/',  // Configuración para desarrollo local/Azure Static Web Apps
   // 2. Añadimos la resolución de alias
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
   },
-  base: '/',
+
   server: {
     host: false,
     port: 5173
