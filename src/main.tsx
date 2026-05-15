597deb3317fb6919ebcbff723955868c98160c63
adolfoespinozalopez <adolfoespinozalopez@gmail.com>
2026-05-15 12:24:13 -0500

    refactor: update import paths and clean up comments in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
-import { router } from './app/router'
-import { AppProviders } from './app/providers'
+import { router } from '@/app/router'
+import { AppProviders } from '@/app/providers'
 
 //Hoja de estilo personalizada
 import './assets/styles/sidebar-fix.css'
@@ -10,12 +10,13 @@ import './assets/styles/sidebar-fix.css'
 // scroll bar
 import 'simplebar-react/dist/simplebar.min.css';
 
-//Hoja de estilos globales (Tailwind) 
+//Hoja de estilos globales 
 import './main.css'
 
-import ScrollTop from './components/ScrollTop'
+//project imports
+import ScrollTop from '@/components/ScrollTop'
 
-// SE IMPORTA LAS FUENTES AQUÍ
+// Fuentes
 import '@fontsource/public-sans/400.css';
 import '@fontsource/public-sans/500.css';
 import '@fontsource/public-sans/600.css';
